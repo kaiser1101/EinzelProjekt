79bc1273c31ba6d8109c35dc2f86c4a550e8300b
kaiser.philipp95@icloud.com <130212301+kaiser1101@users.noreply.github.com>
2024-03-02 19:57:55 +0100

    create Layout for NEXUS 7(2012)
    Layout der App erstellt + Farbe angepasst + getestet

--- app/src/main/java/com/example/networktest/MainActivity.java
+++ app/src/main/java/com/example/networktest/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.networktest;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +13,17 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText editTxtEingabeMatNr;
+    private TextView txtViewAntwortServer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
-    }
+
+        editTxtEingabeMatNr = findViewById(R.id.editTxt_eingabeMatNr);
+        txtViewAntwortServer = findViewById(R.id.txtView_antwortServer);
+        Button buttonAbschicken = findViewById(R.id.btn_abschicken);
+
+        }
 }
